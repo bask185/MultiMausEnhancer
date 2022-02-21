b50721a8acc15cdd82fbd8cc2655efa4ef6f7459
bask185 <sebastiaan_knippels4@hotmail.com>
2022-02-21 22:21:19 +0100

    I can succesfully use a pseudo point address to set entire streets loaded from EEPROM. Now I must teachin the points

--- MultiMausEnhancer.ino
+++ MultiMausEnhancer.ino
@@ -10,40 +10,64 @@
 #define  F1_F4  0x00
 #define  F5_F8  0x40
 #define F9_F10  0x80
+#define CURVED 0x8000
+#define STRAIGHT 0x0000
 
-#define BASE_ADDRESS 500 
+
+#define BASE_ADDRESS 100 
+#define MAX_ADDRESS 1024
 
 #ifndef debug
 XpressNetMasterClass Xnet ;
 #endif
 
-const int nPointsPerStreet = 20 ;
+const int nPointsPerStreet = 10 ;
 
 uint8   knob ;
 volatile uint16 eeAddress  = 0 ;
 
 unsigned long long prevState ; // 64 bits
 
-uint16  points[ nPointsPerStreet ] ;
-uint8   pointIndex ;
+volatile uint16  points[ nPointsPerStreet ] ;
+volatile uint8   pointIndex = 100 ;      // something high
 
 
 void settingPoints()
 {
-    if( pointIndex) > nPointsPerStreet
+    if( pointIndex > nPointsPerStreet ) return ;
+
+    REPEAT_MS( 500 )
+    {
+    nextPoint:
+        uint16 pointNumber = points[ pointIndex ] & 0x03FF ;
+        uint8  state       = points[ pointIndex ] >> 15 ;
+        pointIndex ++ ;
+
+        if( pointIndex > nPointsPerStreet ) return ;
+        //if( pointNumber >= MAX_ADDRESS )   goto nextPoint ;
+
+        #ifndef debug
+        Xnet.SetTrntPos( pointNumber - 1, state, 1 ) ;                      // set new state
+        delay(20) ;
+        Xnet.SetTrntPos( pointNumber - 1, state, 0 ) ;
+        #endif
+
+    } END_REPEAT
 }
 void setStreet( uint8 streetNumber )
 {
-    uint16 eeAddress = streetNumber * nPointsPerStreet * 2 ;                    // calculate address
-
-    pointIndex = 0 ;                                                            // reset this index for setting a street
-    EEPROM.get( eeAddress, points ) ;                                           // fetch array from EEPROM
+    pinMode(13, INPUT_PULLUP );
+    uint16 eeAddress = streetNumber * nPointsPerStreet * 2 ;                      // calculate address
+   
+    EEPROM.get( eeAddress, points ) ;                                             // fetch array from EEPROM
+    pointIndex = 0 ;                                                                // reset this index for setting a street
 }
-void notifyXNetTrnt( uint16_t Address, uint8_t data )
+void notifyXNetTrnt( uint16_t Address, uint8_t data )                               // setting point 101, gives us 100 back
 {
-    if( Address <= BASE_ADDRESS || Address > (BASE_ADDRESS + 20) ) return ;
+    pinMode(13, INPUT_PULLUP );
+    if( Address < BASE_ADDRESS || Address >= (BASE_ADDRESS + 20) ) return ;
 
-    setStreet( Address - BASE_ADDRESS ) 
+    setStreet( Address - BASE_ADDRESS ) ;
 }
 
 
@@ -205,13 +229,53 @@ void notifyXNetLocoFunc4( uint16_t Address, uint8_t Func4 )  //F20 F19 F18 F17 F
 
 void setup()
 {
+    uint16_t eeAddress = 0 ;
+    EEPROM.put( eeAddress++, CURVED   | 1   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 2   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 3   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 4   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 5   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 6   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 7   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 8   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 9   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 10  ) ; eeAddress++ ;
+
+    EEPROM.put( eeAddress++, STRAIGHT | 1   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 2   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 3   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 4   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, STRAIGHT | 5   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 6   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 7   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 8   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 9   ) ; eeAddress++ ;
+    EEPROM.put( eeAddress++, CURVED   | 10  ) ; eeAddress++ ;
+
+    // points[ 0 ] = CURVED   | 1 ;
+    // points[ 1 ] = CURVED   | 2 ;
+    // points[ 2 ] = CURVED   | 3 ;
+    // points[ 3 ] = CURVED   | 4 ;
+    // points[ 4 ] = CURVED   | 5 ;
+    // points[ 5 ] = STRAIGHT | 6 ;
+    // points[ 6 ] = STRAIGHT | 7 ;
+    // points[ 7 ] = STRAIGHT | 8 ;
+    // points[ 8 ] = STRAIGHT | 9 ;
+    // points[ 9 ] = STRAIGHT | 10 ;
+
+    // points[ 10 ] = 0xFFFF ;
+
+    // pointIndex = 100 ;
+
     #ifndef debug
     Xnet.setup( Loco28, RS485DIR ) ;
     #else
     Serial.begin( 115200 ) ;
-    for( int i = 0 ; i < 50 ; i ++ )
+    for( uint16 eeAddress = 0 ; eeAddress < 100 ; eeAddress += 2 )
     {
-        int8 b = EEPROM.read(i) ;
+        uint16_t b ;
+        
+        EEPROM.get( eeAddress, b ) ;
         Serial.print( b ) ; Serial.print("   "); Serial.print( b, HEX ) ; Serial.print("   "); Serial.println( b, BIN ) ;
         
     }
@@ -224,6 +288,7 @@ void loop()
 
     #ifndef debug
     Xnet.update() ;
+    settingPoints() ;
 
     #else
     for( knob = 0 ; knob <= 4 ; knob ++ )
