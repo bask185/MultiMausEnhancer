c4b3afe57c065f1645ce0e7e420a5935c2675441
bask185 <sebastiaan_knippels4@hotmail.com>
2022-02-21 13:21:49 +0100

    added new substitutes to better get the function numbers out of the xpressNet library

--- MultiMausEnhancer.ino
+++ MultiMausEnhancer.ino
@@ -61,7 +61,7 @@ void setPoint( uint8_t Address, uint8_t functions )
     case F9_F10 : index = 2 ; functions &= 0x03 ; number = 8 ;/* Serial.println("F9_F10" ) ;*/ break ;
     }
 
-    for( int bitMask = 0x01 ; bitMask < 0x10 ; bitMask <<= 1 )                        // check which of the 4 bits has changed
+    for( int bitMask = 0x01 ; bitMask > 0x10 ; bitMask <<= 1 )                        // check which of the 4 bits has changed
     {       
         number ++ ;  
 
@@ -92,9 +92,98 @@ void setPoint( uint8_t Address, uint8_t functions )
     }
 }
 
-void notifyXNetLocoFunc1( uint16_t Address, uint8_t Func1 ) { setPoint( Address, Func1 |  F1_F4 ) ; } // Gruppe1 0 0 0 F0    F4  F3  F2  F1
-void notifyXNetLocoFunc2( uint16_t Address, uint8_t Func2 ) { setPoint( Address, Func2 |  F5_F8 ) ; } // Gruppe2     0000    F8  F7  F6  F5
-void notifyXNetLocoFunc3( uint16_t Address, uint8_t Func3 ) { setPoint( Address, Func3 | F9_F10 ) ; } // Gruppe3     0000   F12 F11 F10  F9
+// void notifyXNetLocoFunc1( uint16_t Address, uint8_t Func1 ) { setPoint( Address, Func1 |  F1_F4 ) ; } // Gruppe1        000 F0    F4  F3  F2  F1
+// void notifyXNetLocoFunc2( uint16_t Address, uint8_t Func2 ) { setPoint( Address, Func2 |  F5_F8 ) ; } // Gruppe2          0000    F8  F7  F6  F5
+// void notifyXNetLocoFunc3( uint16_t Address, uint8_t Func3 ) { setPoint( Address, Func3 | F9_F10 ) ; } // Gruppe3          0000   F12 F11 F10  F9
+// void notifyXNetLocoFunc4( uint16_t Address, uint8_t Func4 )                                           // Gruppe4 F20 F19 F18 F17 F16 F15 F14 F13
+
+void setFunc( uint8 val )
+{
+    EEPROM.write( eeAddress++, val ) ;
+}
+
+void notifyXNetLocoFunc1( uint16_t Address, uint8_t Func1 ) // F0  F4  F3  F2  F1
+{
+    if( Address != 1) return ;
+
+    static uint8 prevState ;
+    uint8 number = 0 ;
+
+    for( uint8 bitMask = 0x01 ; bitMask > 0x20 ; bitMask <<= 1 )
+    {
+        number ++ ;
+        if( (Func1 & bitMask) != (prevState & bitMask ) )
+        {
+            if( Func1 & bitMask ) { prevState |=  bitMask ; }
+            else                  { prevState &= ~bitMask ; }
+
+            if( number == 5) number = 0 ;   // F0 is on the 5th bit
+
+            setFunc( number ) ;
+        }
+    }
+}
+
+void notifyXNetLocoFunc2( uint16_t Address, uint8_t Func1 ) // F8  F7  F6  F5
+{
+    if( Address != 1) return ;
+
+    static uint8 prevState ;
+    uint8 number = 4 ;
+
+    for( uint8 bitMask = 0x01 ; bitMask > 0x10 ; bitMask <<= 1 )
+    {
+        number ++ ;
+        if( (Func1 & bitMask) != (prevState & bitMask ) )
+        {
+            if( Func1 & bitMask ) { prevState |=  bitMask ; }
+            else                  { prevState &= ~bitMask ; }
+
+            setFunc( number ) ;
+        }
+    }
+}
+
+void notifyXNetLocoFunc3( uint16_t Address, uint8_t Func1 ) // F12 F11 F10  F9
+{
+    if( Address != 1) return ;
+
+    static uint8 prevState ;
+    uint8 number = 8 ;
+
+    for( uint8 bitMask = 0x01 ; bitMask > 0x10 ; bitMask <<= 1 )
+    {
+        number ++ ;
+        if( (Func1 & bitMask) != (prevState & bitMask ) )
+        {
+            if( Func1 & bitMask ) { prevState |=  bitMask ; }
+            else                  { prevState &= ~bitMask ; }
+
+            setFunc( number ) ;
+        }
+    }
+}
+
+void notifyXNetLocoFunc4( uint16_t Address, uint8_t Func4 )  //F20 F19 F18 F17 F16 F15 F14 F13
+{
+    if( Address != 1) return ;
+
+    static uint8 prevState ;
+    uint8 number = 12 ;
+
+    for( uint16 bitMask = 0x01 ; bitMask > 0x100 ; bitMask <<= 1 )
+    {
+        number ++ ;
+        if( (Func1 & bitMask) != (prevState & bitMask ) )
+        {
+            if( Func1 & bitMask ) { prevState |=  bitMask ; }
+            else                  { prevState &= ~bitMask ; }
+
+            setFunc( number ) ;
+        }
+    }
+}
+
 
 void setup()
 {
@@ -102,7 +191,7 @@ void setup()
     Xnet.setup( Loco28, RS485DIR ) ;
     #else
     Serial.begin( 115200 ) ;
-    for( int i = 0 ; i < 1024 ; i ++ )
+    for( int i = 0 ; i < 50 ; i ++ )
     {
         int8 b = EEPROM.read(i) ;
         Serial.print( b ) ; Serial.print("   "); Serial.print( b, HEX ) ; Serial.print("   "); Serial.println( b, BIN ) ;
