c3079c15a19b11688f45fae3f51cfd380b80b2b8
bask185 <sebastiaan_knippels4@hotmail.com>
2022-04-28 13:18:11 +0200

    did routing of PCB design. I must investigate if I want an extra I2C port to be used with I2C to create extra buttons to this system

--- src/i2cEEPROM.h
+++ src/i2cEEPROM.h
@@ -1,1 +1,18 @@
-i2cEEPROM.h
+#include <Arduino.h>
+#include "macros.h"
+
+class i2cEEPROM
+{
+public:
+    i2cEEPROM() ;
+    void  put( uint16, uint8* ) ;
+    void  get( uint16, uint8* ) ;
+
+private:
+    uint16 eeAddress ;
+} ;
+
+
+Hey Ronald, 
+
+Bedankt voor het aanbod.
